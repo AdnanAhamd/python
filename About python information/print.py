eb6ff8487026a75e7c1e8f4810aa49021fbae68e
unknown <adnanahmad7731@gmail.com>
2025-04-20 13:07:59 +0500

    print value

---
+++ About python information/print.py
@@ -0,0 +1,18 @@
+# basic print value
+print("hello world")
+
+
+
+
+print('''     
+
+      *** Adnan Ahmad ***
+      *** Python programmer ***
+
+
+ ''')
+
+name = "Adnan Ahamd"
+language = "pythan"
+
+print(f"hello {name} is {language} programmer")
